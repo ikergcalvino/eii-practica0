07e9443d15f169cd8cd5e0eb7b0d5990d491aec9
Xoan C. Pardo <xoan.pardo@udc.gal>
2021-09-06 13:29:53 +0200

    Modificado o script bootstrap.sh para evitar entradas duplicadas en /etc/hosts

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,8 +10,15 @@ update-locale LANG=es_ES.UTF-8 LC_MESSAGES=POSIX
 
 # Editar /etc/hosts
 # IMPORTANTE: usa a nomenclatura indicada nas prácticas
-echo >> /etc/hosts
-echo "# XXXXXXXXXX-cluster" >> /etc/hosts
+# Se non hai entradas de execucións previas
+if [[ ! $(grep "192.168.1" "/etc/hosts") ]]; then
+ # Inserir as liñas iniciais 
+ echo >> /etc/hosts
+ echo "# XXXXXXXXXX-cluster" >> /etc/hosts
+else
+ # Eliminar as entradas de execucións previas
+ sed -i "/192.168.1/d" /etc/hosts
+fi
 echo "192.168.1.2 XXXXXXXXXX-master" >> /etc/hosts
 echo "192.168.1.3 XXXXXXXXXX-slave" >> /etc/hosts
-echo "192.168.1.4 XXXXXXXXXX-spare" >> /etc/hosts
+echo "192.168.1.4 XXXXXXXXXX-spare" >> /etc/hostsi
